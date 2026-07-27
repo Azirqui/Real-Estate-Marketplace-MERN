1a02a67783b9f40fa5e7dcbeff3910f68e030608
Ch. Noman Ahmad <mr.noman0095@gmail.com>
2026-07-27 10:05:00 +0500

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route';
+import authRouter from './routes/auth.route';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Connected successfully to MongoDB");
@@ -10,9 +11,11 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running");
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
