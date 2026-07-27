fe801eb4efb467f8aacec62a5b3405e0ba366b46
Ch. Noman Ahmad <mr.noman0095@gmail.com>
2026-07-27 09:48:26 +0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Connected successfully to MongoDB");
@@ -14,3 +15,4 @@ app.listen(3000, () => {
   console.log("Server is running");
 })
 
+app.use('/api/user', userRouter);
