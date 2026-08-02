8593c353be3cf5f518241aecede2dc6adc32d517
Ch. Noman Ahmad <mr.noman0095@gmail.com>
2026-08-02 12:18:47 +0500

    update the header and make the profile page private

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 
 export default function Profile() {
-  const handleGoogleClick = async () => {
-    try {
-      const response = await fetch('/api/auth/google', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      const data = await response.json()
-      window.location.href = data.url
-    } catch (error) {
-      console.error('Error during Google OAuth:', error)
-    }
-  }
+
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Continue with google</button>
+
+    <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+
   )
 }
