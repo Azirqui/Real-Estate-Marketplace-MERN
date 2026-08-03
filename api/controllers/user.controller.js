f229d92441011bc6504fcdd9eee7374471880b1b
Ch. Noman Ahmad <mr.noman0095@gmail.com>
2026-08-03 09:50:42 +0500

    create user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,33 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt";
+
 export const test = (req, res) => {
     res.json({
         message: 'Hello World!',
     })
 };
+
+export const updateUser = async (req, res, next) => {
+    try {
+        if (req.user.id !== req.params.id) return res.status(401).json({ message: 'You can only update your own account!' });
+        try {
+            if (req.body.password) {
+                req.body.password = bcrypt.hashSync(req.body.password, 10);
+            }
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                }
+            }, { new: true });
+            const { password, ...rest } = updatedUser;
+            res.status(200).json(rest);
+
+        } catch (err) {
+            next(err);
+        }
+    } catch (error) {
+        next(error);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Connected successfully to MongoDB");
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running");
@@ -23,6 +25,8 @@ app.use('/api/auth', authRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({ success: false, 
-    statusCode, message });
+  return res.status(statusCode).json({
+    success: false,
+    statusCode, message
+  });
 })
