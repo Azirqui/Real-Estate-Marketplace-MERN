67da25d6c6be803aa36bd666d237b02595208560
nomanahmad3 <noman.ahmad@softmatic.io>
2026-08-06 19:58:17 +0500

    create get user listings api routes

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js"
+import Listing from "../models/listing.model.js"
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
     res.json({
@@ -51,3 +53,15 @@ export const deleteUser = async(req, res, next) => {
         next(error);
     }
 }
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
