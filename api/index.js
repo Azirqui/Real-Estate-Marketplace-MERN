f6d601972980bc2d547364631f570d7a4b6d7e07
Ch. Noman Ahmad <mr.noman0095@gmail.com>
2026-08-29 10:41:15 +0500

    fix build problems

--- api/index.js
+++ api/index.js
@@ -16,29 +16,31 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const __dirname = path.resolve();
 
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-app.listen(3000, () => {
-  console.log("Server is running");
-})
-
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
-    statusCode, message
+    statusCode,
+    message
   });
-})
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
